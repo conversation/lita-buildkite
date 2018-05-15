0daf415add1e6f30d261b878440bd818edc02927
nickbrowne <nickbrowne@users.noreply.github.com>
2018-05-15 14:51:33 +1000

    Simplify messages

--- spec/build_status_report_spec.rb
+++ spec/build_status_report_spec.rb
@@ -16,25 +16,12 @@ RSpec.describe BuildStatusReport do
     context "days_since_last_failure greater than prev_days_since_last_failure" do
       let(:days_since_last_failure) { 2 }
       let(:prev_days_since_last_failure) { 1 }
+      let(:most_successful_days) { 0 }
 
-      context "prev_days_since_last_failure greater than most_successful_days" do
-        let(:most_successful_days) { 0 }
-
-        it "yields a happy message" do
-          expect { |b|
-            BuildStatusReport.new(event, days_since_last_failure, prev_days_since_last_failure, most_successful_days, &b)
-          }.to yield_with_args("tc broke a new record! 2 days without a failure 🎉")
-        end
-      end
-
-      context "prev_days_since_last_failure less than most_successful_days" do
-        let(:most_successful_days) { 3 }
-
-        it "yields a happy message" do
-          expect { |b|
-            BuildStatusReport.new(event, days_since_last_failure, prev_days_since_last_failure, most_successful_days, &b)
-          }.to yield_with_args("tc is 2 days without a failure. The current record is 3. Aim for the top!")
-        end
+      it "yields a happy message" do
+        expect { |b|
+          BuildStatusReport.new(event, days_since_last_failure, prev_days_since_last_failure, most_successful_days, &b)
+        }.to yield_with_args("tc is 2 days without a failure")
       end
     end
 
@@ -58,7 +45,7 @@ RSpec.describe BuildStatusReport do
         it "yields a sad message" do
           expect { |b|
             BuildStatusReport.new(event, days_since_last_failure, prev_days_since_last_failure, most_successful_days, &b)
-          }.to yield_with_args("tc failed after 2 days. The current record is 3. Needs improvement!")
+          }.to yield_with_args("tc failed after 2 days, previous best was 3")
         end
       end
     end
